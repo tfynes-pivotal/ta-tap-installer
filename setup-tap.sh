30794e746d167c2e29969c31003827297c0b928b
Mark Fynes <tfynes@vmware.com>
2023-08-29 14:37:58 -0400

    updates from thomas

---
+++ setup-tap.sh
@@ -0,0 +1,17 @@
+# SCRIPT TO COMPLETE TAP INSTALLATION
+# REQUIRES following CLIs
+# age
+# kubectl ( pre-connected to target k8s cluster with tanzu-essentials pre-installed thereon)
+#
+# environment variables used
+# TanzuNet Creds
+#
+# DockerHub Creds
+#
+# GitHub Creds
+#
+# tanzu-registry-secret.sops.yaml
+# user-registry-dockerconfig.sops.yaml
+# workload-git-auth.sops.yaml
+# sso-credentials-secret.sops.yaml
+# tap-sensitive-values.sops.yaml 
