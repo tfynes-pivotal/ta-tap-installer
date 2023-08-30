99ead2b18f1481a6aa71a50b08df6c496d6bac49
Mark Fynes <tfynes@vmware.com>
2023-08-30 14:05:31 -0400

    descriptions in spaces with on/off. moved to-encrypt secrets

--- secrets-to-seal/copy-encrypted-secrets.sh
+++
@@ -1,3 +0,0 @@
-cp ./config/tanzu-registry-secret.sops.yaml      ../cluster-config/config/
-cp ./config/user-registry-dockerconfig.sops.yaml ../cluster-config/config/
-cp ./config/workload-git-auth.sops.yaml          ../cluster-config/config/
