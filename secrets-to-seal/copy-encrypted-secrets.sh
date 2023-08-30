31861e9645e0e2bed8445adbfac8684d5b53de57
Mark Fynes <tfynes@vmware.com>
2023-08-30 09:31:03 -0400

    okta secret

---
+++ secrets-to-seal/copy-encrypted-secrets.sh
@@ -0,0 +1,3 @@
+cp ./config/tanzu-registry-secret.sops.yaml      ../cluster-config/config/
+cp ./config/user-registry-dockerconfig.sops.yaml ../cluster-config/config/
+cp ./config/workload-git-auth.sops.yaml          ../cluster-config/config/
