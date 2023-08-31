ced29ec89dc5849ec34d8099b0672688325167c5
Mark Fynes <tfynes@vmware.com>
2023-08-31 10:53:44 -0400

    missing sops file.. age key content needed in accelerator

--- accelerator-assets/configure-deployment.sh
+++ accelerator-assets/configure-deployment.sh
@@ -1,5 +1,7 @@
 
-./secrets-to-seal/encrypt-secrets.sh
+pushd ./secrets-to-seal
+./encrypt-secrets.sh
+popd
 
 
 cp ./secrets-to-seal/config/tanzu-registry-secret.sops.yaml ../clusters/taplab/cluster-config/config/
@@ -7,6 +9,7 @@ cp ./secrets-to-seal/config/user-registry-dockerconfig.sops.yaml ../clusters/tap
 cp ./secrets-to-seal/config/workload-git-auth.sops.yaml          ../clusters/taplab/cluster-config/config/
 
 cp ./secrets-to-seal/dependent-resources/sso-credentials-secret.sops.yaml ../clusters/taplab/cluster-config/dependent-resources/
+cp ./secrets-to-seal/tanzu-sync/tanzu-sync-values.sops.yaml ../clusters/taplab/cluster-config/dependent-resources/
 
 cp ./ytt-outputs/tap-non-sensitive-values.yaml             ../clusters/taplab/cluster-config/values/
-cp ./secrets-to-seal/values/tap-sensitive-values.sops.yaml ../clusters/taplab/cluster-config/values/
+cp ./secrets-to-seal/values/tap-sensitive-values.sops.yaml ../clusters/taplab/tanzu-sync/app/sensitive-values/
