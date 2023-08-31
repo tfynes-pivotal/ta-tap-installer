afbb3bbf53a9a5d1c42c57b85926cc8d95bf7aa7
Mark Fynes <tfynes@vmware.com>
2023-08-31 13:30:09 -0400

    sops file copy fix

--- accelerator-assets/configure-deployment.sh
+++ accelerator-assets/configure-deployment.sh
@@ -9,7 +9,6 @@ cp ./secrets-to-seal/config/user-registry-dockerconfig.sops.yaml ../clusters/tap
 cp ./secrets-to-seal/config/workload-git-auth.sops.yaml          ../clusters/taplab/cluster-config/config/
 
 cp ./secrets-to-seal/dependent-resources/sso-credentials-secret.sops.yaml ../clusters/taplab/cluster-config/dependent-resources/
-cp ./secrets-to-seal/tanzu-sync/tanzu-sync-values.sops.yaml ../clusters/taplab/cluster-config/dependent-resources/
+cp ./secrets-to-seal/tanzu-sync/tanzu-sync-values.sops.yaml ../clusters/taplab/tanzu-sync/app/sensitive-values/
 
 cp ./ytt-outputs/tap-non-sensitive-values.yaml             ../clusters/taplab/cluster-config/values/
-cp ./secrets-to-seal/values/tap-sensitive-values.sops.yaml ../clusters/taplab/tanzu-sync/app/sensitive-values/
