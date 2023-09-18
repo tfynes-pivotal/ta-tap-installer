958419e02b91f7902cf9c51daae21d4486958872
Mark Fynes <tfynes@vmware.com>
2023-09-18 17:46:00 -0400

    update tanzunet secret to support custom upstream registry

---
+++ unused/image-copy.sh
@@ -0,0 +1,16 @@
+export IMGPKG_REGISTRY_HOSTNAME=harbor.harborlab.fynesy.net
+export IMGPKG_REGISTRY_USERNAME=admin
+export IMGPKG_REGISTRY_PASSWORD='Harbor12345!'
+export TAP_VERSION=1.6.3
+#! export TBS_VERSION=1.10.9
+
+docker login registry.tanzu.vmware.com
+
+imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:$TAP_VERSION        --to-repo $IMGPKG_REGISTRY_HOSTNAME/tap/tap-packages --include-non-distributable-layers       
+
+
+
+#! imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/full-tbs-deps-package-repo:1.10.8   --to-repo=$IMGPKG_REGISTRY_HOSTNAME/tap/tbs-full-deps
+#!imgpkg copy -b registry.tanzu.vmware.com/spring-cloud-gateway-for-kubernetes/scg-package-repository:2.0.0 --to-tar=/Users/thomasfynes/PIVOTAL/TAP/tap15offline/scg.tar
+#!imgpkg copy -b registry.tanzu.vmware.com/packages-for-vmware-tanzu-data-services/tds-packages:1.7.0       --to-tar=/Users/thomasfynes/PIVOTAL/TAP/tap15offline/tds.tar
+#!imgpkg copy -b registry.tanzu.vmware.com/p-rabbitmq-for-kubernetes/tanzu-rabbitmq-package-repo:1.4.1      --to-tar=/Users/thomasfynes/PIVOTAL/TAP/tap15offline/rmq.tar
