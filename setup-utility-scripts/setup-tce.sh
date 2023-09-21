8769266c3da308548af0dc436da612fbd7236f79
Mark Fynes <tfynes@vmware.com>
2023-09-21 11:46:12 -0400

    .

--- setup-utility-scripts/setup-tce.sh
+++ setup-utility-scripts/setup-tce.sh
@@ -15,5 +15,5 @@ export INSTALL_REGISTRY_HOSTNAME= 'registry.tanzu.vmware.com'
 export INSTALL_REGISTRY_USERNAME= #@ data.values.tanzunetUsername
 export INSTALL_REGISTRY_PASSWORD= #@ data.values.tanzunetPassword
 export INSTALL_BUNDLE=$INSTALL_REGISTRY_HOSTNAME/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:2f538b69c866023b7d408cce6f0624c5662ee0703d8492e623b7fce10b6f840b 
-#@ end
+#@ end 
 ./install_tce.sh
