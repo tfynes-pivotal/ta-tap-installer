9f97ca89c576df716512f0f492def2fe35847921
Mark Fynes <tfynes@vmware.com>
2024-01-16 12:04:58 -0500

    .

--- accelerator-assets/multi-cluster/setupRepos.sh
+++ accelerator-assets/multi-cluster/setupRepos.sh
@@ -16,12 +16,3 @@ gh repo create DELIVERABLES-REPO-NAME --private
 # create workload repo's per namespace
 gh repo create DELIVERIES-REPO-NAME --private
 
-
-
-git init -b main
-git add .
-git commit -am "."
-gh repo create tap-gitops-a3full2 --private
-git remote add origin https://github.com/tfynes-pivotal/tap-gitops-a3full2
-git branch -M main
-git push -u origin main
